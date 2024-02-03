3a990b2123dd91af805289c73aad2e04dc8552b5
Maryna Pylypchenko <maryna.pylypchenko@gmail.com>
2024-02-03 13:34:07 -0800

    the the server was created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const app =express();
+const cors= require("cors")
+const Port =3030;
+const videoRoutes= require("./routes/videos")
+
+
+//configuration
+require("dotenv").config();
+const PORT =process.env.PORT || 3030;
+
+//Middleware
+app.use(cors())
+app.use(express.static("public/images"))
+app.use(express.json());
+
+//routes
+app.use("/videos", videoRoutes);
+
+app.listen(Port, ()=>{
+    console.log(`The server is listening the ${Port}`)
+})
+
